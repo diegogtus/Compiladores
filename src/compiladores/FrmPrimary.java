df920cb56eba1432e63075f2559c166c2126528d
Diego Dominguez <alejandro.diego.gt@gmail.com>
2020-08-18 22:55:06 -0600

    Fixed the bug
    Now its working fine.

--- src/compiladores/FrmPrimary.java
+++ src/compiladores/FrmPrimary.java
@@ -40,11 +40,11 @@ public class FrmPrimary extends javax.swing.JFrame {
 
         jButton2 = new javax.swing.JButton();
         javax.swing.JButton btnBrowser = new javax.swing.JButton();
-        javax.swing.JTextField txtPath = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         txta_output = new javax.swing.JTextArea();
         jScrollPane2 = new javax.swing.JScrollPane();
         txta_input = new javax.swing.JTextArea();
+        txtPath = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
@@ -65,10 +65,6 @@ public class FrmPrimary extends javax.swing.JFrame {
             }
         });
 
-        txtPath.setEditable(false);
-        txtPath.setText("Source Paht");
-        txtPath.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
-
         txta_output.setEditable(false);
         txta_output.setColumns(20);
         txta_output.setRows(5);
@@ -79,32 +75,35 @@ public class FrmPrimary extends javax.swing.JFrame {
         txta_input.setRows(5);
         jScrollPane2.setViewportView(txta_input);
 
+        txtPath.setEditable(false);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(90, 90, 90)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(144, 144, 144)
-                .addComponent(jButton2)
-                .addGap(135, 135, 135)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnBrowser)
+                        .addGap(18, 18, 18)
+                        .addComponent(txtPath, javax.swing.GroupLayout.PREFERRED_SIZE, 565, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(14, 14, 14))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(144, 144, 144)
+                        .addComponent(jButton2)
+                        .addGap(135, 135, 135)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(168, 168, 168))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(btnBrowser)
-                .addGap(18, 18, 18)
-                .addComponent(txtPath, javax.swing.GroupLayout.PREFERRED_SIZE, 545, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(202, 202, 202))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(104, 104, 104)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtPath, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnBrowser))
+                    .addComponent(btnBrowser)
+                    .addComponent(txtPath, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(37, 37, 37)
@@ -222,11 +221,11 @@ private void escribir() {
             }
         });
     }
-private javax.swing.JTextField txtPath;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextField txtPath;
     private javax.swing.JTextArea txta_input;
     private javax.swing.JTextArea txta_output;
     // End of variables declaration//GEN-END:variables
